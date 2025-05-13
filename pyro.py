c955df4140ca71105ff47c1013a877c0549e2560
Vijay Vishwakarma <fishgolden0023@gmail.com>
2025-05-13 23:59:48 +0530

    Second Update

--- pyro.py
+++ pyro.py
@@ -26,8 +26,8 @@ def init():
         return
     else:
         os.mkdir(os.path.join(dir, '.minigit'))
-        os.mkdir(os.path.join(file, 'refs'))
-        os.mkdir(os.path.join(file, 'refs', 'heads'))
+        # os.mkdir(os.path.join(file, 'refs'))
+        # os.mkdir(os.path.join(file, 'refs', 'heads'))
         os.mkdir(os.path.join(file, 'objects'))
         os.mkdir(os.path.join(file, 'commits'))
         os.mkdir(os.path.join(file, 'status'))
@@ -48,6 +48,10 @@ def init():
         with open(os.path.join(file, 'logs', 'logs.json'), 'a+') as log:
             data = {'logger': []}
             log.write(json.dumps(data))
+
+        with open(os.path.join(file, 'commits', 'commits.json'), 'w') as commit:
+            data = {'commits': []}
+            commit.write(json.dumps(data))
             
 
 def add(*args):
@@ -77,13 +81,12 @@ def add(*args):
                         logger_data['logger'].append(data)
                         log.seek(0)
                         log.truncate()
-                        log.write(json.dumps(logger_data))
+                        log.write(json.dumps(logger_data, indent=3))
 
                     files.add(f)
  
                 else:
                     print(f"File {f} not found in the current directory.")
-                    # return
             
         else:
             """
@@ -101,7 +104,7 @@ def add(*args):
                     print(data)
                     log.seek(0)
                     log.truncate()
-                    log.write(json.dumps(logger_data))
+                    log.write(json.dumps(logger_data, indent=3))
                     
                 files.add(f)
 
@@ -159,24 +162,6 @@ def status():
 
             data['untracked files'] = (data['untracked files'] - files) | (files - ((data['tracked files'] & files) | (data['untracked files'] & files))) | (data['untracked files'] & files)
             MODIFICATION = True
-            # if len(data['untracked files']) != 0:
-            #     untracked_deleted_files = data['untracked files'] - files
-            #     untracked_added_files = files - data['untracked files']
-
-            #     if untracked_deleted_files or untracked_added_files:
-            #         MODIFICATION = True
-            #         print(f"Untracked data modified")
-            #         data['untracked files'] = (data['untracked files'] - untracked_deleted_files) | untracked_added_files
-
-            # if len(data['tracked files']) != 0:
-            #     tracked_deleted_files = data['tracked files'] - files
-            #     tracked_added_files = files - data['tracked files']
-
-            #     if len(tracked_deleted_files) != 0 or len(tracked_added_files) != 0:
-            #         MODIFICATION = True    
-            #         print(f"Tracked data modified:")
-            #         data['tracked files'] = (data['tracked files'] - tracked_deleted_files)
-            #         # data['untracked files'] = (data['untracked files'] | tracked_added_files)
 
             if MODIFICATION:
                 data = {'tracked files': list(data['tracked files']), 'untracked files': list(data['untracked files'])}
@@ -191,6 +176,77 @@ def status():
         print("Repository not initialized. Please run 'init' first.")
         return
 
+def hash_file(filename):
+    """
+    Generate a hash for the file.
+    """
+    sha256 = hashlib.shake_128()
+    h = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        context = f.read()
+        sha256.update(context)
+        return sha256.hexdigest(10)
+
+
+def commit(message):
+    """
+    Commit the changes to the repository.
+    """
+    dir = os.getcwd()
+    file = '.minigit'
+
+    if os.path.isdir(file):
+        with open(os.path.join(file, 'status', 'stat.json'), 'r+') as stat:
+            data = json.load(stat)
+            files = data['tracked files']
+
+            """
+            Get the tracked files and update thier log to commited
+            """
+
+            for f in files:
+                with open(os.path.join(file, 'logs', 'logs.json'), 'r+') as log:
+                    log.seek(0)
+                    logger_data = json.load(log)
+                    data = {
+                        'file name': f,
+                        'latest': datetime.now().isoformat(),
+                        'status': 'commited',
+                        'commited': True
+                    }
+                    logger_data['logger'].append(data)
+                    print(data)
+                    log.seek(0)
+                    log.truncate()
+                    log.write(json.dumps(logger_data, indent=3))
+
+            files_commit_map = {}
+            for f in files:
+                hash_val = hash_file(f)
+
+                objects_dir = os.path.join(file, 'objects', f)
+                os.makedirs(objects_dir, exist_ok=True)
+                with open(f, 'rb') as original_file:
+                    content = original_file.read()
+                with open(os.path.join(objects_dir, hash_val), 'wb') as hashed_file:
+                    hashed_file.write(content)
+
+                files_commit_map[f] = hash_val
+
+            with open(os.path.join(file, 'commits', 'commits.json'), 'r+') as commit:
+                commit.seek(0)
+                commit_data = json.load(commit)
+                data = {
+                    'timestamp': datetime.now().isoformat(),
+                    'message': message,
+                    'files':files_commit_map
+                }
+                commit_data['commits'].append(data)
+                print(data)
+                commit.seek(0)
+                commit.truncate()
+                commit.write(json.dumps(commit_data, indent=3))
+
 
 if __name__ == "__main__":
     init()
@@ -203,8 +259,37 @@ if __name__ == "__main__":
         txt.write('This is a test file.')
     status()
     print("\n")
+    print("H!")
     add()
+    print("H!!")
     print("\n")
     status()
+    print("\n")
+    commit('First commit')
+    print("\n")
+    status()
+    with open(os.path.join(os.getcwd(), 'bison1.txt'), 'w+') as txt:
+        txt.write('This is a test file.')
+    print("\n")
+    status()
+    print("\n")
+    add()
+    print("\n")
+    commit('Second commit')
+    print("\n")
+    with open(os.path.join(os.getcwd(), 'bison1.txt'), 'r+') as txt:
+        data = txt.read()
+        # print(data)
+        data = data + 'Some updated content'
+        txt.seek(0)
+        txt.truncate()
+        txt.write(data)
+    print("\n")
+    status()
+    print("\n")
+    add()
+    print("\n")
+    commit('Last commit')
+    print("\n")
     # Add more functionality as needed
     # For example, you can add functions to add files, commit changes, etc.
